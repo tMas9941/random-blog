88023ace6359d3f3b158948a62e981e5e44b6dca
Tamas <tamasm9941@gmail.com>
2025-10-19 17:18:40 +0200

    add share button

---
+++ frontend/src/components/posts/ShareButton.jsx
@@ -0,0 +1,16 @@
+import { changePopupData, popupResults } from "../../global/popupHandler";
+import Button from "../buttons/Button";
+import SvgComponent from "../misc/SvgComponent";
+
+export default function ShareButton({ className, postId }) {
+    const handleClick = () => {
+        const link = window.location.host + "/posts/" + postId;
+        navigator.clipboard.writeText(link);
+        changePopupData("Link copied to clipboard! ", popupResults.warning);
+    };
+    return (
+        <Button title={"Edit post"} text={"Share"} className={"ms-auto " + className} onClick={handleClick}>
+            <SvgComponent size={25} name={"share"} className={"fill-accent stroke-accent"} />
+        </Button>
+    );
+}

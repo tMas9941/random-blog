0e8f6542e44bdb49f759726f7a6d4357b1140e81
Tamas <tamasm9941@gmail.com>
2025-12-02 00:12:12 +0100

    changed own post/comment design

--- frontend/src/components/misc/Avatar.jsx
+++ frontend/src/components/misc/Avatar.jsx
@@ -3,7 +3,7 @@ import { userSignal } from "../../global/userData";
 
 const defaultUrl = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1755695929/default_avatar.jpg`;
 
-export default function Avatar({ size = 80, url = defaultUrl, userId }) {
+export default function Avatar({ size = 80, url = defaultUrl, isOwn = false }) {
     return (
         <div
             style={{
@@ -14,7 +14,7 @@ export default function Avatar({ size = 80, url = defaultUrl, userId }) {
                 borderWidth: Math.min(2.5, size * 0.04),
             }}
             className={`relative flex items-center justify-center rounded-[15%] overflow-hidden select-none ${
-                userId === userSignal.value?.id && "border-primary"
+                isOwn && "border-primary"
             }`}
         >
             <img className="z-2 w-full h-full pointer-events-none " src={url} />

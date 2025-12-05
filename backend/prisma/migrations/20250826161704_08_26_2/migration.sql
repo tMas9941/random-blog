2a8badc5983be047ac6261acdcfa2e524d99632c
Tamas <tamasm9941@gmail.com>
2025-12-05 13:39:14 +0100

    remove migrations

--- backend/prisma/migrations/20250826161704_08_26_2/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "CommentVotes" DROP CONSTRAINT "CommentVotes_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "CommentVotes" ADD CONSTRAINT "CommentVotes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/prisma/migrations/20251205114901_12_05/migration.sql
+++ backend/prisma/migrations/20251205114901_12_05/migration.sql
@@ -1,10 +1,20 @@
+-- CreateEnum
+CREATE TYPE "PermissionActions" AS ENUM ('CREATE', 'READ', 'UPDATE', 'DELETE');
+
+-- CreateEnum
+CREATE TYPE "PermissionSubjects" AS ENUM ('POSTS', 'COMMENTS', 'SETTINGS');
+
+-- CreateEnum
+CREATE TYPE "PermissionTarget" AS ENUM ('OWN', 'ALL');
+
 -- CreateTable
 CREATE TABLE "Posts" (
     "id" TEXT NOT NULL,
-    "title" VARCHAR(100) NOT NULL,
-    "content" VARCHAR(300) NOT NULL,
+    "title" VARCHAR(150) NOT NULL,
+    "content" VARCHAR(1000) NOT NULL,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "authorId" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Posts_pkey" PRIMARY KEY ("id")
 );
@@ -13,12 +23,22 @@ CREATE TABLE "Posts" (
 CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
     "username" VARCHAR(30) NOT NULL,
-    "password" VARCHAR(30) NOT NULL,
+    "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "introduction" VARCHAR(300) NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profiles" (
+    "id" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "introduction" VARCHAR(300) NOT NULL,
+    "profileUserId" TEXT NOT NULL,
+
+    CONSTRAINT "Profiles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -69,14 +89,48 @@ CREATE TABLE "CommentVotes" (
     CONSTRAINT "CommentVotes_pkey" PRIMARY KEY ("userId","commentId")
 );
 
+-- CreateTable
+CREATE TABLE "Permissions" (
+    "action" "PermissionActions" NOT NULL,
+    "subject" "PermissionSubjects" NOT NULL,
+
+    CONSTRAINT "Permissions_pkey" PRIMARY KEY ("action","subject")
+);
+
+-- CreateTable
+CREATE TABLE "Roles" (
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Roles_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "RolePermissions" (
+    "action" "PermissionActions" NOT NULL,
+    "subject" "PermissionSubjects" NOT NULL,
+    "target" "PermissionTarget" NOT NULL,
+    "roleName" TEXT NOT NULL,
+
+    CONSTRAINT "RolePermissions_pkey" PRIMARY KEY ("roleName","action","subject")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Profiles_profileUserId_key" ON "Profiles"("profileUserId");
+
 -- AddForeignKey
-ALTER TABLE "Posts" ADD CONSTRAINT "Posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Posts" ADD CONSTRAINT "Posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Users" ADD CONSTRAINT "Users_role_fkey" FOREIGN KEY ("role") REFERENCES "Roles"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profiles" ADD CONSTRAINT "Profiles_profileUserId_fkey" FOREIGN KEY ("profileUserId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comments" ADD CONSTRAINT "Comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -100,7 +154,13 @@ ALTER TABLE "PostVotes" ADD CONSTRAINT "PostVotes_userId_fkey" FOREIGN KEY ("use
 ALTER TABLE "PostVotes" ADD CONSTRAINT "PostVotes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CommentVotes" ADD CONSTRAINT "CommentVotes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CommentVotes" ADD CONSTRAINT "CommentVotes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CommentVotes" ADD CONSTRAINT "CommentVotes_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermissions" ADD CONSTRAINT "RolePermissions_roleName_fkey" FOREIGN KEY ("roleName") REFERENCES "Roles"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermissions" ADD CONSTRAINT "RolePermissions_action_subject_fkey" FOREIGN KEY ("action", "subject") REFERENCES "Permissions"("action", "subject") ON DELETE CASCADE ON UPDATE CASCADE;

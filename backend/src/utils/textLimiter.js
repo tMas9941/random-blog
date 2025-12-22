8877881ce2450e891baaf85edf6d2379d4189a71
Tamas <tamasm9941@gmail.com>
2025-12-22 16:57:35 +0100

    update textLimiter

--- backend/src/utils/textLimiter.js
+++ backend/src/utils/textLimiter.js
@@ -6,12 +6,23 @@ export const textLimiterData = {
     Tags: { name: 20 },
 };
 
-export default function textLimiter(data, maxLength) {
-    let limitedData;
-    if (typeof data === "string") {
+export default function textLimiter(data, maxLength = 0, model) {
+    let limitedData = {};
+    if (model in textLimiterData) {
+        // limit the values of an object basaed on textLimiterData
+        const entries = Object.entries(data);
+        for (let entry of entries) {
+            if (entry[0] in textLimiterData[model]) {
+                limitedData[entry[0]] = entry[1].substr(0, textLimiterData[model][entry[0]]);
+            } else {
+                limitedData[entry[0]] = entry[1];
+            }
+        }
+    } else if (typeof data === "string") {
         limitedData = data.substr(0, maxLength);
     } else if (Array.isArray(data)) {
         limitedData = data.map((item) => item.substr(0, maxLength));
     }
+    console.log({ limitedData });
     return limitedData;
 }
